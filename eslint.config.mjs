8a9fc06bccecac64eb05924877703270ae19e735
z3rtxblade <egor50311@gmail.com>
2026-01-29 18:06:38 +0300

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,13 @@
-import stylistic from '@stylistic/eslint-plugin'
+import stylistic from '@stylistic/eslint-plugin';
 export default {
   plugins: {
-    '@stylistic': stylistic,
+    stylistic,
   },
   rules: {
-    semi: ['error', 'never'], // идентичные кавычки вокруг 'semi'
-    'comma-dangle': ['error', 'always-multiline'],
-    'arrow-parens': ['error', 'always'],
-    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
-    'eol-last': ['error', 'always'],
-    'no-multiple-empty-lines': ['error', { max: 0 }],
-    'quote-props': ['error', 'as-needed'],
+    'stylistic/quote-props': ['error', 'consistent'],
+    'stylistic/brace-style': ['error', '1tbs', { allowSingleLine: false }],
+    'stylistic/arrow-parens': ['error', 'as-needed'],
+    'stylistic/comma-dangle': ['error', 'always-multiline'],
+    'stylistic/no-multiple-empty-lines': ['error', { max: 0 }],
   },
-}
+};
